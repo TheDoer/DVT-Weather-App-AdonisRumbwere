c920e654a3906cf9f03edc7d7387c8a92e5d533d
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2019-12-09 15:40:28 +0200

    Initial Commit

---
+++ DVT-WeatherApp-AdonisRumbwere/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DVT-WeatherApp-AdonisRumbwere
+//
+//  Created by adonis rumbwere on 9/12/2019.
+//  Copyright © 2019 Adonis Rumbwere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
